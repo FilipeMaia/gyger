2786adcb84f65663dae6bd882ee685da0f8a153e
Filipe Maia <filipe.c.maia@gmail.com>
2023-10-24 10:15:16 +0200

    Correct documentation

--- sheetjet/discovery.py
+++ sheetjet/discovery.py
@@ -28,8 +28,12 @@ def discover(devices = ['VCMini', 'TG5012A', 'MXP7970', 'MXP9970'], config_file 
     Returns
     -------
     dict
-        Dictionary of the PySerial `ListPortInfo <https://pyserial.readthedocs.io/en/latest/tools.html#serial.tools.list_ports.ListPortInfo>` for each of the devices specified.
+        A dictionary with the `DeviceInfo` of each of the devices specified.
     
+    See Also
+    --------
+    sheetjet.discovery.DeviceInfo
+
     """
     ret = None
     if(load_config):
@@ -111,6 +115,21 @@ def read_config(config_file, check_against_ports = True):
 
 
 class DeviceInfo:
+    """
+    Stores basic information about a serial port and the hardware attached to it.
+    This information is derived from pySerial's `ListPortInfo` class.
+    https://pyserial.readthedocs.io/en/latest/tools.html#serial.tools.list_ports.ListPortInfo
+
+    Attributes
+    ----------
+    device : str
+        The device name/path, e.g. `COM4` on Windows or `/dev/ttyUSB0` under Linux.
+        Corresponds to the `device` attribute in `ListPortInfo`.
+    hwid : str
+        An identifier for the hardware attached on the device.
+        Should help us to later identify if the same hardware is still plugged in.
+    
+    """
     def __init__(self, device, hwid):
         self.device = device
         self.hwid = hwid
