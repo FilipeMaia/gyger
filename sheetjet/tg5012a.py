61c6e5595f51ab4f15e4eb9efe3f2fd4819eba7b
Filipe Maia <filipe.c.maia@gmail.com>
2024-01-10 14:12:55 +0100

    Add more functions

--- sheetjet/tg5012a.py
+++ sheetjet/tg5012a.py
@@ -80,12 +80,33 @@ class TG5012A:
     # Continuous Carrier Wave Commands
     def wave(self, set = "PULSE"):
         """Sets the output waveform"""
+        valid_waveforms = ["SINE", "SQUARE", "TRIANG", "RAMP", "PULSE", "NOISE", "ARB"]
+        if set not in valid_waveforms:
+            raise ValueError("Invalid waveform. It should be one of %s" % (valid_waveforms))
         return self.set("WAVE", str(set))
     
     def frequency(self, set = 1):
         """Sets the output frequency in Hz"""
         return self.set("FREQ", str(set))
     
+    def period(self, set = 1):
+        """Sets the output period in seconds"""
+        return self.set("PER", str(set))
+    
+    def amplitude_range(self, set = "AUTO"):
+        """Sets the output amplitude range"""
+        valid = ["AUTO", "HOLD"]
+        if set not in valid:
+            raise ValueError("Invalid amplitude range. It should be one of %s" % (valid))
+        return self.set("AMPLRNG", str(set))
+    
+    def amplitude_unit(self, set = "VPP"):
+        """Sets the output amplitude unit"""
+        valid = ["VPP", "VRMS", "DBM"]
+        if set not in valid:
+            raise ValueError("Invalid amplitude unit. It should be one of %s" % (valid))
+        return self.set("AMPUNIT", str(set))    
+
     def amplitude(self, set = 1):
         """Sets the output amplitude, in volts"""
         return self.set("AMPL", str(set))
@@ -104,12 +125,43 @@ class TG5012A:
     
     def output(self, set = "ON"):
         """Sets the output on, off, normal or invert"""
+        valid = ["ON", "OFF", "NORMAL", "INVERT"]
+        if set not in valid:
+            raise ValueError("Invalid output. It should be one of %s" % (valid))
         return self.set("OUTPUT", set)
     
     def output_load(self, set = 50):
         """Sets the output load, in Ohms"""
+        if set != 'OPEN' and (set < 1 or set > 10000):
+            raise ValueError("Invalid output load. It should be between 1 and 10000 or OPEN")
         return self.set("ZLOAD", str(set))
 
+    def square_symmetry(self, set = 50):
+        """Sets the square wave symmetry, in percent"""
+        if set < 0 or set > 100:
+            raise ValueError("Invalid square symmetry. It should be between 0 and 100")
+        return self.set("SQRSYMM", str(set))
+    
+    def ramp_symmetry(self, set = 50):
+        """Sets the ramp wave symmetry, in percent"""
+        if set < 0 or set > 100:
+            raise ValueError("Invalid ramp symmetry. It should be between 0 and 100")
+        return self.set("RMPSYMM", str(set))
+    
+    def sync_output(self, set = "OFF"):
+        """Sets the sync output on or off"""
+        valid = ["ON", "OFF"]
+        if set not in valid:
+            raise ValueError("Invalid sync output. It should be one of %s" % (valid))
+        return self.set("SYNCOUT", set)
+    
+    def sync_type(self, set = "AUTO"):
+        """Sets the sync type"""
+        valid = ["AUTO", "CARRIER", "MODULATION", "SWEEP", "BURST", "TRIGGER"]
+        if set not in valid:
+            raise ValueError("Invalid sync type. It should be one of %s" % (valid))
+        return self.set("SYNCTYPE", set)
+        
     def phase(self, set = 0):
         """Sets the output phase, in degrees"""
         return self.set("PHASE", str(set))
@@ -124,9 +176,33 @@ class TG5012A:
         """Sets the pulse frequency, in Hz"""
         return self.set("PULSFREQ", str(set))
     
+    def pulse_period(self, set = 1):
+        """Sets the pulse period, in seconds"""
+        return self.set("PULSPER", str(set))
+    
     def pulse_width(self, set = 1):
         """Sets the pulse width, in seconds"""
         return self.set("PULSWID", str(set))
+    
+    def pulse_symmetry(self, set = 50):
+        """Sets the pulse symmetry, in percent"""
+        if set < 0 or set > 100:
+            raise ValueError("Invalid pulse symmetry. It should be between 0 and 100")
+        return self.set("PULSSYMM", str(set))
+    
+    def pulse_edge(self, set = 0):
+        """Set the pulse waveform edges (positive and negative edge) in seconds. 
+        Value zero sets to the minimum value allowed."""
+        return self.set("PULSEDGE", str(set))
+
+    def pulse_range(self, set = 1):
+        """Set the pulse rise and fall range to 1, 2 or 3.
+        1 - sets the range from 5ns to 99.9ns
+        2 - sets the range from 100ns to 1.999us
+        3 - sets the range from 2us to 40us"""
+        if set not in [1,2,3]:
+            raise ValueError("Invalid pulse range. It should be 1,2 or 3")
+        return self.set("PULSRANGE", str(set))
 
     def pulse_rise(self, set = 0.001):
         """Sets the pulse rise time, in seconds"""
@@ -139,7 +215,75 @@ class TG5012A:
     def pulse_delay(self, set = 0):
         """Sets the pulse delay, in seconds"""
         return self.set("PULSDLY", str(set))
+    
+    # Dual-channel Function Commands
 
+    def amplitude_coupling(self, set = "ON"):
+        """Sets the amplitude coupling"""
+        valid = ["ON", "OFF"]
+        if set not in valid:
+            raise ValueError("Invalid amplitude coupling. It should be one of %s" % (valid))
+        return self.set("AMPLCPLNG", set)
+    
+    def output_coupling(self, set = "ON"):
+        """Sets the output coupling"""
+        valid = ["ON", "OFF"]
+        if set not in valid:
+            raise ValueError("Invalid output coupling. It should be one of %s" % (valid))
+        return self.set("OUTPUTCPLNG", set)
+    
+    def frequency_coupling(self, set = "ON"):
+        """Sets the frequency coupling"""
+        valid = ["ON", "OFF"]
+        if set not in valid:
+            raise ValueError("Invalid frequency coupling. It should be one of %s" % (valid))
+        return self.set("FRQCPLSWT", set)
+    
+    def frequency_coupling_type(self, set = "RATIO"):
+        """Sets the frequency coupling type"""
+        valid = ["RATIO", "OFFSET"]
+        if set not in valid:
+            raise ValueError("Invalid frequency coupling type. It should be one of %s" % (valid))
+        return self.set("FRQCPLTYP", set)
+
+    def frequency_coupling_ratio(self, set = 1):
+        """Sets the frequency coupling ratio"""
+        return self.set("FRQCPLRAT", str(set))
+    
+    def frequency_coupling_offset(self, set = 0):
+        """Sets the frequency coupling offset in Hz"""
+        return self.set("FRQCPLOFS", str(set))
+    
+    def pulse_frequency_coupling(self, set = "ON"):
+        """Sets the pulse frequency coupling"""
+        valid = ["ON", "OFF"]
+        if set not in valid:
+            raise ValueError("Invalid pulse frequency coupling. It should be one of %s" % (valid))
+        return self.set("PLSFRQCPLSWT", set)
+    
+    def pulse_frequency_coupling_type(self, set = "RATIO"):
+        """Sets the pulse frequency coupling type"""
+        valid = ["RATIO", "OFFSET"]
+        if set not in valid:
+            raise ValueError("Invalid pulse frequency coupling type. It should be one of %s" % (valid))
+        return self.set("PLSFRQCPLTYP", set)
+    
+    def pulse_frequency_coupling_ratio(self, set = 1):
+        """Sets the pulse frequency coupling ratio"""
+        return self.set("PLSFRQCPLRAT", str(set))
+    
+    def pulse_frequency_coupling_offset(self, set = 0):
+        """Sets the pulse frequency coupling offset in Hz"""
+        return self.set("PLSFRQCPLOFS", str(set))
+    
+    def tracking(self, set = "EQUAL"):
+        """Set channel tracking to OFF, EQUAL or INVERT"""
+        valid = ["OFF", "EQUAL", "INVERT"]
+        if set not in valid:
+            raise ValueError("Invalid tracking. It should be one of %s" % (valid))
+        return self.set("TRACKING", set)
+    
+    
     # System and Status Commands
     def query_error(self):
         """Query and clear Query Error Register"""
@@ -172,6 +316,10 @@ class TG5012A:
     def wait_for_completion(self):
         """Waits for the instrument to complete its tasks"""
         return self.query("*OPC?")
+    
+    def beep(self):
+        """Sounds the instrument's beeper"""
+        return self.set("BEEP")
     
     def local(self):
         """Sets the instrument to local mode"""
